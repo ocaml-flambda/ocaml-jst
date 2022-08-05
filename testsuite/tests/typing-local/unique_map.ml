db7b14ae91a52108a52d3afbe4e365907cb120e0
Anton Lorenzen <anlorenzen@janestreet.com>
2022-08-05 17:48:20 +0100

    Fix unique_map test

--- testsuite/tests/typing-local/unique_map.ml
+++ testsuite/tests/typing-local/unique_map.ml
@@ -1,36 +1,33 @@
 (* TEST
    flags += "-extension unique"
- * expect *)
+ * native
+   reference = "${test_source_directory}/unique_map.reference"
+ *)
 
 type 'a list =
   | Cons of { hd : 'a; tl : 'a list }
   | Nil
-[%%expect{|
-type 'a list = Cons of { hd : 'a; tl : 'a list; } | Nil
-|}]
 
 let rec map f (unique_ xs) =
   match xs with
   | Nil -> xs
   | Cons ({hd;tl} as xs) -> Cons { unique_ xs with hd = f hd; tl = map f tl }
-[%%expect{|
->> Fatal error: Bytegen.comp_primitive
-Uncaught exception: Misc.Fatal_error
 
-|}]
+let () =
+  let xs = Cons { hd = 1; tl = Cons { hd = 2; tl = Cons { hd = 3; tl = Nil } } } in
 
-let test = Cons { hd = 1; tl = Cons { hd = 2; tl = Cons { hd = 3; tl = Nil } } }
-[%%expect{|
-val test : int list =
-  Cons {hd = 1; tl = Cons {hd = 2; tl = Cons {hd = 3; tl = Nil}}}
-|}]
+  let prebefore = Gc.allocated_bytes () in
+  let before = Gc.allocated_bytes () in
+  let _ = Sys.opaque_identity (map (fun x -> x + 1) xs) in
+  let after = Gc.allocated_bytes () in
+  let delta =
+    int_of_float ((after -. before) -. (before -. prebefore))
+    / (Sys.word_size/8)
+  in
+  let msg =
+    match delta with
+    | 0 -> "No Allocation"
+    | n -> "Allocation"
+  in
+  Printf.printf "%15s: %s\n" "List.map" msg
 
-let test2 = map (fun x -> x + 1)
-    (Cons { hd = 1; tl = Cons { hd = 2; tl = Cons { hd = 3; tl = Nil } } })
-[%%expect{|
-Line 1, characters 12-15:
-1 | let test2 = map (fun x -> x + 1)
-                ^^^
-Error: Unbound value map
-Hint: Did you mean max?
-|}]
