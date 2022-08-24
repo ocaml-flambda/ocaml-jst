4bcd6223c87bfe27c925e3135c40420dd2b65771
Antal Spector-Zabusky <antal.b.sz@gmail.com>
2022-08-24 17:43:21 -0400

    Add more side effect tests

--- testsuite/tests/comprehensions/list_comprehensions_side_effects.ml
+++ testsuite/tests/comprehensions/list_comprehensions_side_effects.ml
@@ -33,15 +33,19 @@ let show_as_tuple show_item xs =
   |> String.concat ", "
 ;;
 
-let report f =
+let out3 (i, j, k) = say "  %d, %d, %d;" i j k;;
+
+let out6 (i, j, k, x, y, z) = say "  %d, %d, %d, %d, %d, %d;" i j k x y z;;
+
+let report out f =
   let result = f () in
   say "";
   say "result = [";
-  List.iter (fun (i, j, k) -> say "  %d, %d, %d;" i j k) result;
+  List.iter out result;
   say "]"
 ;;
 
-report (fun () ->
+report out3 (fun () ->
   [ begin
       say ">>> i = %d, j = %d, k = %d" i j k;
       i, j, k
@@ -49,22 +53,38 @@ report (fun () ->
       for k = (say ">> first (from)"; -1) downto (say ">> second (to)"; -3)
       when (say "> i = %d, j = %d | when even i" i j; i mod 2 = 0)
       for i = (say "first (from)"; 0) to (say "second (to)"; 3)
-      and j in (say "third (and)"; [10;20;30])
+      and j in (say "third (in)"; [10;20;30])
   ]
 )
 ;;
 
 say "";;
 
-report (fun () ->
+report out3 (fun () ->
   [ begin
       say ">>> i = %d, j = %d, k = %d" i j k;
       i, j, k
     end
       for i = (say "first (from)"; 0) to (say "second (to)"; 3)
-      and j in (say "third (and)"; [10;20;30])
+      and j in (say "third (in)"; [10;20;30])
       and k = (say "fourth (downfrom)"; 100) downto (say "fifth (downto)"; 97)
   ]
 )
 ;;
 
+say "";;
+
+report out6 (fun () ->
+  [ begin
+      say ">>> i = %d, j = %d, k = %d, x = %d, y = %d, z = %d" i j k x y z;
+      i, j, k, x, y, z
+    end
+      for x = (say "> first (downfrom)"; 1) downto (say "> second (downto)"; 0)
+      and y in (say "> third (in)"; [20;10])
+      and z = (say "> fourth (from)"; 99) to (say "> fifth (to)"; 100)
+      for i = (say "first (from)"; 0) to (say "second (to)"; 1)
+      and j in (say "third (in)"; [10;20])
+      and k = (say "fourth (downfrom)"; 100) downto (say "fifth (downto)"; 99)
+  ]
+)
+;;
