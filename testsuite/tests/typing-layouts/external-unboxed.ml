24776e5a57216ed4b70116cabbc59aece2c6c26f
Richard Eisenberg <reisenberg@janestreet.com>
2022-12-15 08:49:57 -0500

    A little refactoring to simplify the alloc test

--- testsuite/tests/typing-layouts/external-unboxed.ml
+++ testsuite/tests/typing-layouts/external-unboxed.ml
@@ -7,29 +7,27 @@
 (* Goal: test to make sure that calls to primitive functions do not
    allocate. *)
 
-(* put this in a function to avoid the number showing up in the expect-test. *)
-(* NB: right-to-left evaluation order gets this right *)
-let baseline_allocation () = Gc.allocated_bytes() -. Gc.allocated_bytes()
+let measure_alloc f =
+  (* NB: right-to-left evaluation order gets this right *)
+  let baseline_allocation = Gc.allocated_bytes() -. Gc.allocated_bytes() in
+  let before = Gc.allocated_bytes () in
+  f ();
+  let after = Gc.allocated_bytes () in
+  (after -. before) -. baseline_allocation
 
 [%%expect {|
 success
 |}]
 
 let alloc =
-  let before = Gc.allocated_bytes() in
-  let f = Ufloat.cos #0.0 in
-  let after = Gc.allocated_bytes() in
-  (after -. before) -. baseline_allocation ()
+  measure_alloc (fun () -> let f = Ufloat.cos #0.0 in ())
 
 [%%expect {|
 0
 |}]
 
 let alloc =
-  let before = Gc.allocated_bytes() in
-  let f = Ufloat.(#2.1 + #3.4) in
-  let after = Gc.allocated_bytes() in
-  (after -. before) -. baseline_allocation ()
+  measure_alloc (fun () -> let f = Ufloat.(#2.1 + #3.4) in ())
 
 [%%expect {|
 0
