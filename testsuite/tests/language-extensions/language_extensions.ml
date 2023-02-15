eada1baf8787c858a97481ec1a576dc5ae4e6ccb
Antal Spector-Zabusky <antal.b.sz@gmail.com>
2023-02-15 17:24:16 -0500

    Fix language extension test to use typechecking

--- testsuite/tests/language-extensions/language_extensions.ml
+++ testsuite/tests/language-extensions/language_extensions.ml
@@ -4,14 +4,14 @@
    reference = "${test_source_directory}/reference.txt"
 *)
 
-(* Currently, this test case only works with modular extensions, as they fail
-   during parsing and we test via parsing. *)
-
 (* Change these two variables to change which extension is being tested *)
 let extension            = Clflags.Extension.Comprehensions
 let extension_expression = "[x for x = 1 to 10]"
 
 let extension_name = Clflags.Extension.to_string extension
+let extension_parsed_expression =
+  Parse.expression (Lexing.from_string extension_expression)
+  (* Currently, parsing always succeeds and we only fail during typechecking *)
 
 let report ~name ~text =
   Printf.printf "# %s [%s %s]:\n%s\n\n"
@@ -20,18 +20,19 @@ let report ~name ~text =
     (if Clflags.Extension.is_enabled extension then "enabled" else "disabled")
     text
 
-let parse_with_extension ?(full_name = false) name =
-  let expr =
-    match Parse.expression (Lexing.from_string extension_expression) with
-    | expr -> Some expr
-    | exception (Extensions_parsing.Error.Error _) -> None
+let typecheck_with_extension ?(full_name = false) name =
+  let success =
+    match Typecore.type_expression Env.empty extension_parsed_expression with
+    | _ -> true
+    | exception (Extensions_parsing.Error.Error _) -> false
   in
   report
     ~name:(if full_name
            then name
            else "\"" ^ extension_name ^ "\" extension " ^ name)
-    ~text:(Option.fold
-             ~none:"<syntax error>" ~some:Pprintast.string_of_expression expr)
+    ~text:(if success
+           then "Successfully typechecked \"" ^ extension_expression ^ "\""
+           else "<extension error>")
 ;;
 
 let should_succeed name what f =
@@ -76,38 +77,38 @@ let lift_with with_fn extension = with_fn extension Fun.id;;
 
 (* Test the ground state *)
 
-parse_with_extension "in its default state";
+typecheck_with_extension "in its default state";
 
 (* Disable all extensions for testing *)
 
 List.iter Clflags.Extension.disable Clflags.Extension.all;
-parse_with_extension ~full_name:true "no extensions enabled";
+typecheck_with_extension ~full_name:true "no extensions enabled";
 
 (* Test globally toggling a language extension *)
 
 Clflags.Extension.enable extension;
-parse_with_extension "enabled";
+typecheck_with_extension "enabled";
 
 Clflags.Extension.enable extension;
-parse_with_extension "still enabled";
+typecheck_with_extension "still enabled";
 
 Clflags.Extension.disable extension;
-parse_with_extension "disabled";
+typecheck_with_extension "disabled";
 
 Clflags.Extension.disable extension;
-parse_with_extension "still disabled";
+typecheck_with_extension "still disabled";
 
 Clflags.Extension.set extension ~enabled:true;
-parse_with_extension "enabled via [set]";
+typecheck_with_extension "enabled via [set]";
 
 Clflags.Extension.enable extension;
-parse_with_extension "still enabled, via [set] and [enable]";
+typecheck_with_extension "still enabled, via [set] and [enable]";
 
 Clflags.Extension.set extension ~enabled:false;
-parse_with_extension "disabled via [set]";
+typecheck_with_extension "disabled via [set]";
 
 Clflags.Extension.disable extension;
-parse_with_extension "still disabled, via [set] and [disable]";
+typecheck_with_extension "still disabled, via [set] and [disable]";
 
 (* Test locally toggling a language extension *)
 
@@ -116,31 +117,33 @@ parse_with_extension "still disabled, via [set] and [disable]";
 Clflags.Extension.disable extension;
 
 Clflags.Extension.with_enabled extension (fun () ->
-  parse_with_extension "enabled locally and disabled globally");
+  typecheck_with_extension "enabled locally and disabled globally");
 
 Clflags.Extension.with_disabled extension (fun () ->
-  parse_with_extension "disabled locally and globally");
+  typecheck_with_extension "disabled locally and globally");
 
 Clflags.Extension.with_set extension ~enabled:true (fun () ->
-  parse_with_extension "enabled locally via [with_set] and disabled globally");
+  typecheck_with_extension
+    "enabled locally via [with_set] and disabled globally");
 
 Clflags.Extension.with_set extension ~enabled:false (fun () ->
-  parse_with_extension "disabled locally via [with_set] and also globally");
+  typecheck_with_extension "disabled locally via [with_set] and also globally");
 
 (* Globally enable the language extension *)
 Clflags.Extension.enable extension;
 
 Clflags.Extension.with_disabled extension (fun () ->
-  parse_with_extension "disabled locally and enabled globally");
+  typecheck_with_extension "disabled locally and enabled globally");
 
 Clflags.Extension.with_enabled extension (fun () ->
-  parse_with_extension "enabled locally and globally");
+  typecheck_with_extension "enabled locally and globally");
 
 Clflags.Extension.with_set extension ~enabled:false (fun () ->
-  parse_with_extension "disabled locally via [with_set] and enabled globally");
+  typecheck_with_extension
+    "disabled locally via [with_set] and enabled globally");
 
 Clflags.Extension.with_set extension ~enabled:true (fun () ->
-  parse_with_extension "disabled locally via [with_set] and also globally");
+  typecheck_with_extension "disabled locally via [with_set] and also globally");
 
 (* Test disallowing extensions *)
 
