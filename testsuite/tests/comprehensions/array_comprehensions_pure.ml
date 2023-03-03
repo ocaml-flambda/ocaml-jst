462e77ebb402773e8340e6df9fe4eaa1ea6311f3
Nick Roberts <nroberts02@gmail.com>
2023-03-03 17:08:33 -0500

    Copy tests to all three of array, iarray, list

--- testsuite/tests/comprehensions/array_comprehensions_pure.ml
+++ testsuite/tests/comprehensions/array_comprehensions_pure.ml
@@ -409,3 +409,42 @@ Line 1, characters 23-24:
 Warning 18 [not-principal]: this type-based constructor disambiguation is not principal.
 - : M.t array = [|M.B; M.A; M.A; M.A|]
 |}];;
+
+module type S = sig
+  type t
+  val x : t
+end;;
+
+let t = (module struct
+  type t = int
+  let x = 3
+end : S);;
+[%%expect {|
+module type S = sig type t val x : t end
+val t : (module S) = <module>
+|}];;
+
+let f () =
+  [| M.x for (module M : S) in [| t |] |];;
+[%%expect {|
+Line 2, characters 21-22:
+2 |   [| M.x for (module M : S) in [| t |] |];;
+                         ^
+Error: Modules are not allowed in this pattern.
+|}];;
+
+let f () =
+  [| M.x
+    for (module M :S) in
+      [| (module struct
+          type t = int
+          let x = 3
+         end : S)
+      |]
+  |];;
+[%%expect {|
+Line 3, characters 16-17:
+3 |     for (module M :S) in
+                    ^
+Error: Modules are not allowed in this pattern.
+|}];;
