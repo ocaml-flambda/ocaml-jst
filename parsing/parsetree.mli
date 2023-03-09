6f72bfabd68e63cd9349bd7d775e512f6c2c23b9
Richard Eisenberg <reisenberg@janestreet.com>
2023-03-09 18:02:12 -0500

    Suggestions from Antal

--- parsing/parsetree.mli
+++ parsing/parsetree.mli
@@ -211,7 +211,8 @@ and object_field_desc =
 and pattern =
     {
      ppat_desc: pattern_desc;
-     (** Consider using [Extensions.Pattern.of_ast] before matching on
+     (** (Jane Street specific; delete when upstreaming.)
+         Consider using [Extensions.Pattern.of_ast] before matching on
          this field directly, as the former will detect extension nodes
          correctly. Extensions are encoded as
          [Ppat_tuple [Ppat_extension _; _]]; if your pattern match avoids
@@ -283,7 +284,8 @@ and pattern_desc =
 and expression =
     {
      pexp_desc: expression_desc;
-     (** Consider using [Extensions.Expression.of_ast] before matching on
+     (** (Jane Street specific; delete when upstreaming.)
+         Consider using [Extensions.Expression.of_ast] before matching on
          this field directly, as the former will detect extension nodes
          correctly. Extensions are encoded as
          [Pexp_apply(Pexp_extension _, _)]; if your pattern match avoids
