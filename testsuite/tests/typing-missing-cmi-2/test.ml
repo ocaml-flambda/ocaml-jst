7f4598df4a84228c418eba323515c4780310642b
Antal Spector-Zabusky <antal.b.sz@gmail.com>
2023-04-19 16:43:02 -0400

    Re-enable missing-cmi test

--- testsuite/tests/typing-missing-cmi-2/test.ml
+++ testsuite/tests/typing-missing-cmi-2/test.ml
@@ -1,17 +1,15 @@
 (* TEST
 readonly_files = "foo.mli bar.mli baz.ml"
-* skip
-reason = "Not supporting this use of missing cmis with layouts"
-** setup-ocamlc.byte-build-env
-*** ocamlc.byte
+* setup-ocamlc.byte-build-env
+** ocamlc.byte
 module = "foo.mli"
-**** ocamlc.byte
+*** ocamlc.byte
 module = "bar.mli"
-***** script
+**** script
 script = "rm foo.cmi"
-****** ocamlc.byte
+***** ocamlc.byte
 flags = "-c -i"
 module = "baz.ml"
-ocamlc_byte_exit_status = "0"
-******* check-ocamlc.byte-output
+ocamlc_byte_exit_status = "2"
+****** check-ocamlc.byte-output
 *)
