258ca1f12abffc14c45efa761c0ffc924febe333
Nick Roberts <nroberts@janestreet.com>
2023-10-24 10:57:08 +0100

    Add comment for test that we could remove differences with upstream

--- testsuite/tests/misc/pr7168.ml
+++ testsuite/tests/misc/pr7168.ml
@@ -72,6 +72,11 @@ let rec f x =
   ()
 
 let _ =
+  (* CR ocaml 5 runtime: we should be able
+   * to simplify this test along the same
+   * lines as upstream, as stack overflow
+   * detection is always supported in
+* ocaml 5. *)
   if (Gc.get ()).Gc.stack_limit = 0 then begin
     (* We are in native code. Skip the test because some platforms cannot
        reliably detect stack overflow. *)
